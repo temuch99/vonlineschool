60ea29ab745d5731dcbcb66f816f7255e3465672
temuch por <porubenko99@mail.ru>
2020-11-27 21:06:51 +0300

    add mailer configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,14 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "vonlineschool_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => "vonlineschool.ru" }
+  # config.action_mailer.default_url_options = { :host => "vonlineschool.ru" }
+  config.action_mailer.default_url_options = {:host => 'vonlinecshool.ru'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'vonlinecshool.ru'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
