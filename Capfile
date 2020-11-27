269016ab47e6c8c048c713690b632bfac986999a
temuch por <porubenko99@mail.ru>
2020-11-27 16:39:03 +0300

    capistrano settings

---
+++ Capfile
@@ -0,0 +1,18 @@
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+
+require 'capistrano/copy'
+require 'capistrano/rails'
+require 'capistrano/bundler'
+require 'capistrano/rvm'
+require 'capistrano/puma'
+require 'capistrano/yarn'
+require 'capistrano/webpacker/precompile'
+require 'capistrano/rails/console'
+
+install_plugin Capistrano::Puma
+
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+set :application, "vonlineschool"
+
+set :rails_env, "production"
+set :use_sudo, false
+set :rvm_type, :user
+set :migration_role, :web
+set :conditionally_migrate, true
+set :keep_releases, 5
+set :pty, true
+
+set :user, "temuch"
+set :repo_url, "https://github.com/temuch99/vonlineschool.git"
+
+
+set :assets_roles, [:USER]
+# set :passenger_restart_with_touch, true
+# set :deploy_to, "/home/temuch/apps/vonlineschool"
+
+# set :nvm_type, :user # or :system, depends on your nvm setup
+# set :nvm_map_bins, %w{node npm yarn}
+
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", ".bundle", "node_modules", "public/packs"
+
+append :exclude_dir, "public/uploads log tmp vendor public/assets"
+
+
+set :yarn_target_path, -> { release_path.join('.') } #
+set :yarn_flags, '--production --silent --no-progress'    # default
+set :yarn_roles, :all                                     # default
+set :yarn_env_variables, {}
+
+after 'deploy:publishing', 'deploy:restart'
+after 'deploy', 'deploy:cleanup'
+after 'deploy:updated', 'webpacker:precompile'
