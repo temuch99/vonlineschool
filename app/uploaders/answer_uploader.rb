82ba57457692b975fe6f07982b8f84e394f06e0e
temuch99 <porubenko99@mail.ru>
2021-02-25 23:05:22 +0300

    add tiff jfif and bmp format to extension list

--- app/uploaders/answer_uploader.rb
+++ app/uploaders/answer_uploader.rb
@@ -36,7 +36,7 @@ class AnswerUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png pdf doc docx)
+    %w(jpg jpeg gif png pdf doc docx jfif tiff bmp)
   end
 
   # Override the filename of the uploaded files:
