99fe987f234008e943e7aff28509310882fcdb10
temuch99 <porubenko99@mail.ru>
2021-05-02 20:42:43 +0300

    add percentage to pie diagram

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -8,10 +8,13 @@ class Api::CoursesController < Api::ApiController
 		course = Course.find(params[:id])
 		done_lessons = @user.done_lessons(course.id)
 		lessons = course.lessons.count - done_lessons
+		percent = @user.course_percent(course.id) * 100
 		render json: {
 						title: course.title,
 						undone_lessons: lessons,
-						done_lessons: done_lessons
+						done_lessons: done_lessons,
+						percent: percent,
+						_percent: 100 - percent
 					}.to_json
 	end
 	private
